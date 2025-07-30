b45dcefd6727b366a55bb974f8953877fb155aea
Leonardo Rangel <37275024+leo12wb@users.noreply.github.com>
2025-07-30 17:21:33 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("smile")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Happy)
+})
+radio.setGroup(2)
